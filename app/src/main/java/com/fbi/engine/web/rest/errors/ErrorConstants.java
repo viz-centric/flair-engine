1d4ef135966f0d2390dbdb06e545baf5ade65f86
admin-vizcentric <admin@vizcentric.com>
2020-02-19 21:36:47 +0100

    Migrated error handling

--- app/src/main/java/com/fbi/engine/web/rest/errors/ErrorConstants.java
+++ app/src/main/java/com/fbi/engine/web/rest/errors/ErrorConstants.java
@@ -1,11 +1,16 @@
 package com.fbi.engine.web.rest.errors;
 
+import java.net.URI;
+
 public final class ErrorConstants {
 
-    public static final String ERR_CONCURRENCY_FAILURE = "error.concurrencyFailure";
-    public static final String ERR_ACCESS_DENIED = "error.accessDenied";
-    public static final String ERR_VALIDATION = "error.validation";
-    public static final String ERR_METHOD_NOT_SUPPORTED = "error.methodNotSupported";
-    public static final String ERR_INTERNAL_SERVER_ERROR = "error.internalServerError";
+	public static final String ERR_CONCURRENCY_FAILURE = "error.concurrencyFailure";
+	public static final String ERR_VALIDATION = "error.validation";
+	public static final String PROBLEM_BASE_URL = "https://www.jhipster.tech/problem";
+	public static final URI DEFAULT_TYPE = URI.create(PROBLEM_BASE_URL + "/problem-with-message");
+	public static final URI CONSTRAINT_VIOLATION_TYPE = URI.create(PROBLEM_BASE_URL + "/constraint-violation");
+
+	private ErrorConstants() {
+	}
 
 }
--- app/src/test/java/com/fbi/engine/web/rest/errors/ExceptionTranslatorTestController.java
+++ app/src/test/java/com/fbi/engine/web/rest/errors/ExceptionTranslatorTestController.java
@@ -3,12 +3,11 @@ package com.fbi.engine.web.rest.errors;
 import org.springframework.dao.ConcurrencyFailureException;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class ExceptionTranslatorTestController {
@@ -22,17 +21,12 @@ public class ExceptionTranslatorTestController {
     public void methodArgument(@Valid @RequestBody TestDTO testDTO) {
     }
 
-    @GetMapping("/test/parameterized-error")
-    public void parameterizedError() {
-        throw new CustomParameterizedException("test parameterized error", "param0_value", "param1_value");
+    @GetMapping("/test/missing-servlet-request-part")
+    public void missingServletRequestPartException(@RequestPart String part) {
     }
 
-    @GetMapping("/test/parameterized-error2")
-    public void parameterizedError2() {
-        Map<String, String> params = new HashMap<>();
-        params.put("foo", "foo_value");
-        params.put("bar", "bar_value");
-        throw new CustomParameterizedException("test parameterized error", params);
+    @GetMapping("/test/missing-servlet-request-parameter")
+    public void missingServletRequestParameterException(@RequestParam String param) {
     }
 
     @GetMapping("/test/access-denied")
@@ -40,8 +34,13 @@ public class ExceptionTranslatorTestController {
         throw new AccessDeniedException("test access denied!");
     }
 
+    @GetMapping("/test/unauthorized")
+    public void unauthorized() {
+        throw new BadCredentialsException("test authentication failed!");
+    }
+
     @GetMapping("/test/response-status")
-    public void exceptionWithReponseStatus() {
+    public void exceptionWithResponseStatus() {
         throw new TestResponseStatusException();
     }
 
