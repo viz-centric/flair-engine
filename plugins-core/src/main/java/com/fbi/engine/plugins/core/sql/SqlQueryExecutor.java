dd3c735f0f73798fcbee4f5029ef6df851fdaa16
admin-vizcentric <admin@vizcentric.com>
2020-04-09 22:47:32 +0200

    Added hikari cp

--- plugins-core/src/main/java/com/fbi/engine/plugins/core/sql/SqlQueryExecutor.java
+++ plugins-core/src/main/java/com/fbi/engine/plugins/core/sql/SqlQueryExecutor.java
@@ -7,8 +7,13 @@ import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.SQLTransientConnectionException;
 import java.sql.Statement;
+import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.sql.DataSource;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fbi.engine.api.DataSourceConnection;
@@ -17,7 +22,10 @@ import com.fbi.engine.api.Query;
 import com.fbi.engine.api.QueryExecutor;
 import com.fbi.engine.plugins.core.ResultSetConverter;
 import com.project.bi.exceptions.ExecutionException;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -25,7 +33,11 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public abstract class SqlQueryExecutor implements QueryExecutor {
 
+	private static final Map<ConnectionDataKey, ConnectionDataValue> connections = new ConcurrentHashMap<>();
+
 	private static final String USERNAME = "username";
+
+	private static final String PASSWORD = "password";
 
 	protected final DriverLoadingStrategy strategy;
 
@@ -71,8 +83,14 @@ public abstract class SqlQueryExecutor implements QueryExecutor {
 	@Override
 	public void execute(Query query, Writer writer) throws ExecutionException {
 		final Driver driver = initDriver();
-		try (final Connection c = DriverManager.getConnection(this.connection.getConnectionString(),
-				constructConnectionProperties())) {
+		final String connectionString = this.connection.getConnectionString();
+		final String connectionUsername = this.connection.getConnectionProperties().getProperty(USERNAME);
+		final String connectionPassword = this.connection.getConnectionProperties().getProperty(PASSWORD);
+
+		ConnectionDataValue connectionDataValue = getConnection(connectionString, connectionUsername,
+				connectionPassword);
+
+		try (final Connection c = connectionDataValue.getDataSource().getConnection()) {
 			if (c != null) {
 				try (final Statement statement = c.createStatement()) {
 					statement.execute(query.getQuery());
@@ -86,6 +104,9 @@ public abstract class SqlQueryExecutor implements QueryExecutor {
 				log.error("Failed to make connection! Connection string: {}", connection.getConnectionString());
 				throw new ExecutionException("Failed to create a connection");
 			}
+		} catch (SQLTransientConnectionException e) {
+			log.error("Connection to database timed out, stacktrace: {}", e.getMessage());
+			throw new ExecutionException("Database timed out", e);
 		} catch (SQLException e) {
 			log.error("Connection to database failed, stacktrace: {}", e.getMessage());
 			throw new ExecutionException("Database threw an exception", e);
@@ -95,6 +116,46 @@ public abstract class SqlQueryExecutor implements QueryExecutor {
 		} finally {
 			closeDriver(driver);
 		}
+	}
+
+	private static ConnectionDataValue getConnection(String jdbcUrl, String username, String password) {
+		return connections.computeIfAbsent(new ConnectionDataKey(jdbcUrl, username, password),
+				connectionData -> createConnectionValue(connectionData));
+	}
+
+	private static ConnectionDataValue createConnectionValue(ConnectionDataKey connectionData) {
+		return new ConnectionDataValue(createHikariConnection(connectionData));
+	}
+
+	private static DataSource createHikariConnection(ConnectionDataKey connectionData) {
+		log.info("Creating new hikari data source for {}", connectionData.getJdbcUrl());
+		HikariConfig config = new HikariConfig();
+		config.setReadOnly(true);
+		config.setConnectionTimeout(300_000);
+		config.setMaximumPoolSize(50);
+		config.setMinimumIdle(1);
+		config.setIdleTimeout(60_000);
+		config.setJdbcUrl(connectionData.getJdbcUrl());
+		config.setUsername(connectionData.getUsername());
+		config.setPassword(connectionData.getPassword());
+		config.addDataSourceProperty("cachePrepStmts", "true");
+		config.addDataSourceProperty("prepStmtCacheSize", "250");
+		config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+		return new HikariDataSource(config);
+	}
+
+	@Data
+	@RequiredArgsConstructor
+	private static class ConnectionDataKey {
+		private final String jdbcUrl;
+		private final String username;
+		private final String password;
+	}
+
+	@Data
+	@RequiredArgsConstructor
+	private static class ConnectionDataValue {
+		private final DataSource dataSource;
 	}
 
 }
